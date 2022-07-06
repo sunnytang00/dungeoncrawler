8aa05d1a3e61cafffc15d1c32ed8003096d73ee0
Sunny <z5255035@ad.unsw.edu.au>
2022-07-06 23:57:35 +1000

    changed wall test to staticentity test, created exit class

---
+++ src/main/java/dungeonmania/StaticEntities/Exit.java
@@ -0,0 +1,12 @@
+package dungeonmania.StaticEntities;
+
+import dungeonmania.StaticEntity;
+import dungeonmania.util.Position;
+
+public class Exit extends StaticEntity {
+
+    public Exit(String type, Position position) {
+        super(type, position);
+    }
+    
+}
