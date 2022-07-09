ba1e20b0e8d3991a50a5742bdd6a69a03de78b3a
Sunny <z5255035@ad.unsw.edu.au>
2022-07-09 18:45:22 +1000

    added stub functions for door and spawner

---
+++ src/main/java/dungeonmania/StaticEntities/Door.java
@@ -0,0 +1,35 @@
+// package dungeonmania.StaticEntities;
+
+// import dungeonmania.StaticEntity;
+// import dungeonmania.util.Position;
+
+// public class Door extends StaticEntity {
+
+//     //By default StaticEntity isTraversable = false, so that means all doors cannot be traversed by default
+    
+//     private int keyID;
+
+//     public Door(String type, Position position, Key key) {
+//         super(type, position);
+//         this.keyID = keyID;
+//         setType("door_close");
+        
+//     }
+
+//     public void unlockDoor(Key key) {
+//         //if key id matches door id...
+//         if (key.getsomething == keyID) {
+//             setTraversable(true);
+//             setType("door_open");
+
+//         }
+        
+//     }
+
+//     public int getKeyID() {
+//         return keyID;
+//     }
+
+    
+    
+// }
