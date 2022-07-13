2227f72a617f20134f512497a0b5c331f6c6f88a
Sunny Tang <sunny.tang@student.unsw.edu.au>
2022-07-14 01:59:28 +1000

    Revert "Merge branch 'sunny' into 'master'"

--- src/main/java/dungeonmania/movingEntity/PlayerState.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.movingEntity;
-
-public interface PlayerState {
-
-    public void playerStateChange(Player player);
-
-    public void playerInBattle(Player player, Enemy enemy);
-}
