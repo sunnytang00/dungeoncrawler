376b9fa1981d08988144ac350370af9925d7382c
Han <z5253519@ad.unsw.edu.au>
2022-07-13 16:12:22 +1000

    modify

--- src/main/java/dungeonmania/StaticEntities/Boulder.java
+++ src/main/java/dungeonmania/StaticEntities/Boulder.java
@@ -6,6 +6,8 @@ import dungeonmania.util.Direction;
 
 public class Boulder extends StaticEntity{
 
+    private boolean isOnSwitch;
+
     public Boulder(String type, Position position) {
         super(type, position);
         
@@ -14,5 +16,13 @@ public class Boulder extends StaticEntity{
     public void move(Direction direction) {
         setPosition(getPosition().translateBy(direction));
     }
-    
+
+    public boolean isOnSwitch() {
+        return isOnSwitch;
+    }
+
+    public void setOnSwitch(boolean onSwitch) {
+        isOnSwitch = onSwitch;
+    }
 }
+
