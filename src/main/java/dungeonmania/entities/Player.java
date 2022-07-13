93bd5176fb5b85bbb5ceb0c9e1b4e6f456ae2f0f
Han <z5253519@ad.unsw.edu.au>
2022-07-13 16:13:32 +1000

    modify

--- src/main/java/dungeonmania/entities/Player.java
+++ src/main/java/dungeonmania/entities/Player.java
@@ -3,6 +3,9 @@ package dungeonmania.entities;
 import dungeonmania.Entity;
 import dungeonmania.util.Position;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player extends Entity {
     public Player(String type, Position position) {
         super(type, position);
@@ -11,4 +14,10 @@ public class Player extends Entity {
     public Player(String type, Position position, String colour) {
         super(type, position, colour);
     }
+
+    public List<Entity> getEntities() {
+        List<Entity> entities = new ArrayList<>();
+        return entities;
+    }
 }
+
