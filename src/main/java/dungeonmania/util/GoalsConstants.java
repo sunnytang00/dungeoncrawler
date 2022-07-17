b88a093aa7afc0e2882247ac71a13b8616237dd0
Han <z5253519@ad.unsw.edu.au>
2022-07-17 18:57:29 +1000

    modify goals

---
+++ src/main/java/dungeonmania/util/GoalsConstants.java
@@ -0,0 +1,11 @@
+package dungeonmania.util;
+
+public interface GoalsConstants {
+    String GOALS_EXIT = "exit";
+    String GOALS_TREASURE = "treasure";
+    String GOALS_ENEMY = "enemy";
+    String GOALS_BOULDERS = "boulders";
+    String LOGIC_AND = "AND";
+    String LOGIC_OR = "OR";
+}
+
