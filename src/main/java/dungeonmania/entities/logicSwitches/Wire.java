77584ddde3ce297e1e96d301e715a8699fb44d30
Han <z5253519@ad.unsw.edu.au>
2022-07-27 17:41:28 +1000

    logic item

---
+++ src/main/java/dungeonmania/entities/logicSwitches/Wire.java
@@ -0,0 +1,11 @@
+package dungeonmania.entities.logicSwitches;
+
+import dungeonmania.util.Position;
+
+public class Wire extends LogicItem {
+
+    public Wire(String type, Position position, LogicEnum logic) {
+        super(type, position, logic);
+    }
+}
+
