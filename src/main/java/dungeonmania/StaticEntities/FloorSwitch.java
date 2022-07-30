0b446d580d1fdc9420ccea4008cf7a38d0b8f9b6
Han <z5253519@ad.unsw.edu.au>
2022-07-31 01:11:06 +1000

    modify

--- src/main/java/dungeonmania/StaticEntities/FloorSwitch.java
+++ src/main/java/dungeonmania/StaticEntities/FloorSwitch.java
@@ -1,11 +1,14 @@
 package dungeonmania.StaticEntities;
 
 import dungeonmania.StaticEntity;
+import dungeonmania.entities.logicSwitches.LogicEnum;
 import dungeonmania.util.Position;
 
 public class FloorSwitch extends StaticEntity {
 
     private boolean isTriggered;
+    private LogicEnum logic;
+    private boolean isActivated;
 
     public FloorSwitch(String type, Position position) {
         super(type, position);
@@ -24,5 +27,21 @@ public class FloorSwitch extends StaticEntity {
     public void setTriggered(boolean state) {
         this.isTriggered = state;
     }
-    
+
+    public LogicEnum getLogic() {
+        return logic;
+    }
+
+    public void setLogic(LogicEnum logic) {
+        this.logic = logic;
+    }
+
+    public boolean isActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(boolean activated) {
+        isActivated = activated;
+    }
 }
+
