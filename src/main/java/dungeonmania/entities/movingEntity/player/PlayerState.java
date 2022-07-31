40dcedf248db78d5385ce311479e2c71c0b95ef9
Yuemeng Yin <z5270707@ad.unsw.edu.au>
2022-07-31 16:50:59 +1000

    refactor state pattern

--- src/main/java/dungeonmania/entities/movingEntity/player/PlayerState.java
+++ src/main/java/dungeonmania/entities/movingEntity/player/PlayerState.java
@@ -1,7 +1,13 @@
 package dungeonmania.entities.movingEntity.player;
 
+import dungeonmania.DungeonGame;
+import dungeonmania.DungeonMap;
+import dungeonmania.util.Direction;
+
 public interface PlayerState {
 
     public void playerStateChange(Player player);
+    public void move(Player player, Direction direction, DungeonMap map);
+    public void battle(Player player, DungeonMap map, DungeonGame game);
 
 }
